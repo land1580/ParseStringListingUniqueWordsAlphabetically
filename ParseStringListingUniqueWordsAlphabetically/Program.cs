681991b11c960d3067e6c9f0fc299a0274e64b4c
land1580 <land1580@yahoo.com>
2018-06-21 23:46:46 -0400

    different sort  options

--- ParseStringListingUniqueWordsAlphabetically/Program.cs
+++ ParseStringListingUniqueWordsAlphabetically/Program.cs
@@ -10,17 +10,32 @@ namespace ParseStringListingUniqueWordsAlphabetically
     {
         static void Main(string[] args)
         {
-            string paragraph = "I am a string, ok man? What do you a want me to do I.";
+            string paragraph = "I am a string, ok man? What do you a want why what ok i me to do I.";
             Console.WriteLine(paragraph);
             char[] delimiters = { ' ', '.', ',', '?', '!' };
             string[] uniqueWords = paragraph.Split(delimiters);
             Array.Sort(uniqueWords);
-            
-            foreach (var word in uniqueWords.Distinct())
+
+/*            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            foreach (string word in uniqueWords)
             {
-                Console.WriteLine($"{word}");
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word] += 1;
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                    Console.WriteLine(word);
+                }
             }
-            Console.ReadKey();
+            Console.ReadKey();  */
+
+                        foreach (var word in uniqueWords.Distinct())
+                        {
+                            Console.WriteLine($"{word}");
+                        }
+                        Console.ReadKey();   
         }
     }
 }
